afa87326b7e6f0482f51446aaa214eedf5ae6f1b
rostyslav-m <ros55@ukr.net>
2015-10-03 22:40:09 +0300

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,32 @@
 class User < ActiveRecord::Base
   
-  attr_accessor :password, :password__confirmation
+#  attr_accessible :email, :password, :password_confirmation
+  
+  attr_accessor :password
 
   before_save :encrypt_password
+  
   validates_confirmation_of :password
+
+  validates :password, presence: true,
+            format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}\z/,
+            message: "must be at least 8 characters and include one number one lowercase and uppercase letters" }
+
+  validates :name, uniqueness: true,
+            format: { with: /\A[A-Za-z]+\ [A-Za-z]+\z/,
+            message: "only two words and allows letters with a space" }
+
+  validates :username, presence: true,
+            uniqueness: true
+
+  validates :phone, format: { with: /\A\+?(380(\d{9}))|(7(\d{10}))\z/,
+            message: "use format +7XXXXXXXXXX or +380XXXXXXXXX" }
+
+  validates :email, presence: true,
+            uniqueness: true,
+            length: {:minimum => 5, :maximum => 254},
+            format: { with: /@/,
+            message: "is not email format" }
 
   def self.authenticate(email, password)
     user = find_by_email(email)
